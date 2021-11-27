bcb74c48add3d4fc967fa5e912ac566ebf40a28e
Lee Overy <me@leeovery.com>
2021-11-27 20:48:39 +0000

    WIP

--- app/Exchanges/Coinbase/Processors/CreateFiatDepositsForCardPurchasesProcessor.php
+++ app/Exchanges/Coinbase/Processors/CreateFiatDepositsForCardPurchasesProcessor.php
@@ -5,7 +5,6 @@ namespace App\Exchanges\Coinbase\Processors;
 use App\Amount;
 use App\Contracts\TransactionProcessor;
 use App\Enums\TransactionType;
-use App\Exchanges\Coinbase\Facades\Coinbase;
 use App\Transaction;
 use Brick\Money\Money;
 use Illuminate\Support\Collection;
@@ -14,50 +13,36 @@ class CreateFiatDepositsForCardPurchasesProcessor implements TransactionProcesso
 {
     public function handle(Collection $transactions, callable $next): Collection
     {
-        $paymentMethods = Coinbase::fetchPaymentMethods();
-        if ($paymentMethods->isEmpty()) {
-            return $next($transactions);
-        }
+        return $next(
+            $transactions
+                ->filter(fn(Transaction $transaction) => $transaction->type->is(TransactionType::Trade()))
+                ->filter(function (Transaction $transaction) {
+                    return str(data_get($transaction->rawData, 'details.subtitle', ''))->contains('******');
+                })
+                ->map(function (Transaction $transaction) {
+                    $subtotal = Money::of(
+                        data_get($transaction->rawData, 'buy.subtotal.amount'),
+                        data_get($transaction->rawData, 'buy.subtotal.currency')
+                    );
+                    $fee = Money::of(
+                        data_get($transaction->rawData, 'buy.fee.amount'),
+                        data_get($transaction->rawData, 'buy.subtotal.currency')
+                    );
 
-        return $next($this->createAndMergeDeposits($transactions, $paymentMethods));
-    }
-
-    private function createAndMergeDeposits(
-        Collection $transactions,
-        Collection $paymentMethods
-    ): Collection {
-        return $transactions
-            ->filter(fn(Transaction $transaction) => $transaction->type->is(TransactionType::Trade()))
-            ->filter(function(Transaction $transaction) {
-                dd($transaction);
-            })
-            ->filter(fn(Transaction $transaction) => $paymentMethods
-                ->where('id', data_get($transaction->rawData, 'buy.payment_method.id'))
-                ->where('type', 'worldpay_card')
-                ->isNotEmpty())
-            ->map(function (Transaction $transaction) {
-                $subtotal = Money::of(
-                    data_get($transaction->rawData, 'buy.subtotal.amount'),
-                    data_get($transaction->rawData, 'buy.subtotal.currency')
-                );
-                $fee = Money::of(
-                    data_get($transaction->rawData, 'buy.fee.amount'),
-                    data_get($transaction->rawData, 'buy.subtotal.currency')
-                );
-
-                return (new Transaction)
-                    ->setType(TransactionType::Deposit())
-                    ->setBuyAmount(new Amount(
-                        $subtotal->plus($fee)->getAmount(),
-                        data_get($transaction->rawData, 'buy.subtotal.currency'),
-                    ))
-                    ->setTxDate($transaction->txDate->subSecond())
-                    ->setRawData($transaction->rawData)
-                    ->setStatus($transaction->status)
-                    ->setId($transaction->id)
-                    ->setNotes("Auto-created deposit linked to tx#{$transaction->id}. This was a debit/credit card purchase and this auto tx keeps your fiat accounts balanced.");
-            })
-            ->push(...$transactions)
-            ->values();
+                    return (new Transaction)
+                        ->setType(TransactionType::Deposit())
+                        ->setBuyAmount(new Amount(
+                            $subtotal->plus($fee)->getAmount(),
+                            data_get($transaction->rawData, 'buy.subtotal.currency'),
+                        ))
+                        ->setTxDate($transaction->txDate->subSecond())
+                        ->setRawData($transaction->rawData)
+                        ->setStatus($transaction->status)
+                        ->setId($transaction->id)
+                        ->setNotes("Auto-created deposit linked to tx#{$transaction->id}. This was a debit/credit card purchase and this auto tx keeps your fiat accounts balanced.");
+                })
+                ->push(...$transactions)
+                ->values()
+        );
     }
 }
